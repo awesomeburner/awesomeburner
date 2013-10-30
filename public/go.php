c9d13818c300cb7b9c23134b7dde208ea7490b80
vanzhiganov <vanzhiganov@ya.ru>
2013-10-30 11:25:03 +0400

    update

--- public/go.php
+++ public/go.php
@@ -4,5 +4,12 @@
  * @description: подсчет перехода по ссылке и редирект
  */
 
+$item_id = (isset($_GET['item_id'])) ? trim($_GET['item_id']) : 0;
 
+if ($item_id == 0) {
+	header("location: ./");
+	exit();
+}
 
+// TODO: посчитать переход
+?>
--- public/item.php
+++ public/item.php
@@ -103,12 +103,12 @@ PPH;
 	<head>
 		<meta charset="utf-8" />
 		<title><?=$strItemTitle;?> - BeeBlog.org</title>
-		<link href="css/960.css" rel="stylesheet" type="text/css" />
-        <link href="css/v0.1.css" rel="stylesheet" type="text/css" />
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
-    </head>
-    <body>
-        <div class="container_12" id="header">
+		<link href="/css/960.css" rel="stylesheet" type="text/css" />
+		<link href="/css/v0.1.css" rel="stylesheet" type="text/css" />
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
+	</head>
+	<body>
+		<div class="container_12" id="header">
             <div class="grid_12">
                 RSS-feeds: <span id="getnumderrssfeeds" style="font-weight: 600"></span>
                 Articles: <span id="getnumderarticles" style="font-weight: 600"></span>
@@ -151,16 +151,7 @@ PPH;
 				$('#getnumderrssfeeds').append(data.result.feeds);
 				$('#getnumderarticles').append(data.result.items);
             });
-			
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-24323088-1']);
-			_gaq.push(['_setDomainName', 'beeblog.org']);
-			_gaq.push(['_trackPageview']);
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
 		</script>
+		<script src="/js/google.analitycs.js"></script>
 	</body>
 </html>
