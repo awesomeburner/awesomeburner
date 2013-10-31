aeaa3660164d3f5011e7c75623a232f18fcef58e
vanzhiganov <vanzhiganov@ya.ru>
2013-10-31 13:49:36 +0400

    update bot

--- CRON/rss_spider.php
+++ CRON/rss_spider.php
@@ -3,23 +3,34 @@ ini_set('error_reporting', 1);
 ini_set('display_errors', 1);
 set_time_limit(0);
 
-if (isset ($_GET['debug'])== 1) {
-	define("DEBUG_MODE", true);
-} else {
-	define("DEBUG_MODE", false);
-}
+//if (isset ($_GET['debug'])== 1) {
+//	define("DEBUG_MODE", true);
+//} else {
+//	define("DEBUG_MODE", false);
+//}
 
-include '../WEB-INC/conf.php';
-include '../WEB-INC/class.mysql.php';
-include '../WEB-INC/class.contain.feed.php';
-include '../WEB-INC/class.contain.item.php';
-include '../WEB-INC/class.downloader.php';
-include '../WEB-INC/class.feed.php';
-include '../WEB-INC/class.data.php';
-include '../WEB-INC/class.keyword.php';
-include '../WEB-INC/class.rss_spider.php';
+include "../WEB-INC/conf.php";
+include "../WEB-INC/class.mysql.php";
+include "../WEB-INC/class.api.php";
 
-$cDb = new clsMysql();
+include "../WEB-INC/class.downloader.php";
+include "../WEB-INC/class.data.php";
+include "../WEB-INC/class.contain.feed.php";
+include "../WEB-INC/class.contain.item.php";
+
+$api = new clsApi();
+
+$api->crawler("all", array("limit" => 10));
+
+//include "../WEB-INC/class.contain.feed.php";
+//include "../WEB-INC/class.contain.item.php";
+//include "../WEB-INC/class.downloader.php";
+//include "../WEB-INC/class.feed.php";
+//include "../WEB-INC/class.data.php";
+//include "../WEB-INC/class.keyword.php";
+//include "../WEB-INC/class.rss_spider.php";
+
+/*
 $cFeedSpider = new feed_spider();
 
 $index = (isset($_GET['index'])) ? $_GET['index'] : 1;
@@ -36,6 +47,7 @@ if ($index == 1) {
     for ($i = 0; $rss_num > $i; ++$i) {
 		$cFeedSpider->spider_feed($rss[$i]->feed_id);
     }
-} elseif ($index == 0) {
+} else {//if ($index == 0) {
     $cFeedSpider->spider_all();
 }
+*/
