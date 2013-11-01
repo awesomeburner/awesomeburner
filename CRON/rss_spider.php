1752b3957a0ff29c520e8a45760b3bea246bed34
Vyacheslav Anzhiganov <vanzhiganov@ya.ru>
2013-11-01 23:48:33 +0400

    clean

--- CRON/rss_spider.php
+++ CRON/rss_spider.php
@@ -19,25 +19,3 @@ include "../WEB-INC/class.keyword.php";
 $api = new clsApi();
 
 $api->crawler("all", array("limit" => 10));
-
-/*
-$cFeedSpider = new feed_spider();
-
-$index = (isset($_GET['index'])) ? $_GET['index'] : 1;
-
-if ($index == 1) {
-    $rss = $cFeedSpider->cFeed->getLongTimeIndex($index);
-    $rss_num = count($rss);
-
-    if ($rss_num == 0) {
-	    echo "NO FEEDS";
-	    exit();
-    }
-
-    for ($i = 0; $rss_num > $i; ++$i) {
-		$cFeedSpider->spider_feed($rss[$i]->feed_id);
-    }
-} else {//if ($index == 0) {
-    $cFeedSpider->spider_all();
-}
-*/
