a47db8389a2896401521a1446a7d8db69a6ee010
vanzhiganov <vanzhiganov@ya.ru>
2015-03-30 01:03:01 +0300

    test composer

--- public/index2.php
+++ public/index2.php
@@ -6,7 +6,11 @@
  * Time: 12:48 AM
  */
 include "../application/config.php";
-include "../vendor/autoload.php";
+
+include "../vendor/j4mie/idiorm/idiorm.php";
+include "../vendor/twig/twig/lib/Twig/Autoloader.php";
 
 include "../application/load.idiorm.php";
 include "../application/load.twig.php";
+
+echo "test";
