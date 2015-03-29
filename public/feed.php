351256c909323bfaef5a0f356d48d95247d30a0e
vanzhiganov <vanzhiganov@ya.ru>
2015-03-29 23:56:37 +0300

    fix

--- public/feed.php
+++ public/feed.php
@@ -1,11 +1,10 @@
 <?php
 include '../WEB-INC/conf.php';
-include '../WEB-INC/class.db.php';
+include "../WEB-INC/class.mysql.php";
 include '../WEB-INC/class.contain.feed.php';
 include '../WEB-INC/class.contain.item.php';
 include '../WEB-INC/class.data.php';
 
-$cDb = new db();
 $cData = new data();
 
 $intFeedID	= (isset($_GET['cid'])) ? $_GET['cid'] : false;
